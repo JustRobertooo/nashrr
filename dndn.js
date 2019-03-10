955a39930261bd44b903c7e72e8e5365cb1d46c6
JustRobertooo <40578875+JustRobertooo@users.noreply.github.com>
2019-03-10 09:55:21 +0100

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,39 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+
+حيآكم سيرفرنآ  هنآ كل شي تبيه  , :violin: 
+ آن شآء آلله , :cd:  
+  
+آلرآبط : 
+:arrow_right:  https://discord.gg/MZ4b9hq :arrow_left:
+
+! Faster Avatar.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+حيآكم سيرفرنآ  هنآ كل شي تبيه  , :violin: 
+ آن شآء آلله , :cd:  
+  
+آلرآبط : 
+:arrow_right:  https://discord.gg/MZ4b9hq :arrow_left:
+
+! Faster Avatar.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('NTQwOTExODI2Mjk3NDIxODM1.DzXzOw.DmMuxoynqc5Y2h6ho1thQB8jqJE');
